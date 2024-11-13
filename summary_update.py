c14822b0fb380a2a36d39b858a67b339ed7062b9
nohjuhyeon <njh2720@gmail.com>
2024-11-13 11:27:01 +0900

    Auto Commit.

--- summary_update.py
+++ summary_update.py
@@ -135,26 +135,6 @@ try:
                         format='%(asctime)s - %(levelname)s - %(message)s')
     logging.info("----------------news summarization started----------------") # 스케줄러 시작 로그 기록
     total_update()
-    script_path = folder_path+"git_workflow.sh"
-    argument = "Auto Commit."
-
-    try:
-        # 스크립트 실행
-        result = subprocess.run(
-            [script_path, argument],
-            capture_output=True,  # 표준 출력과 표준 오류를 캡처
-            text=True,            # 출력을 문자열로 처리
-            check=True            # 명령어 실패 시 예외 발생
-        )
-        # 실행 결과 출력
-        print("stdout:", result.stdout)
-        print("stderr:", result.stderr)
-
-    except subprocess.CalledProcessError as e:
-        # 오류 발생 시 출력
-        print("An error occurred while executing the script.")
-        print("stdout:", e.stdout)
-        print("stderr:", e.stderr)
 
 except (KeyboardInterrupt, SystemExit):
     print("summarization shut down.")
