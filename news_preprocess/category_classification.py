76789dde36e621f4d69ff2f34972644b75d07de5
nohjuhyeon <njh2720@gmail.com>
2024-11-28 10:31:51 +0900

    push

--- news_preprocess/category_classification.py
+++ news_preprocess/category_classification.py
@@ -29,12 +29,11 @@ def load_model_from_drive(device):
         temp_model_path = tmp_file.name
         
         print("모델 다운로드 중...")
-        file_id = "1-3lWWtppGyMgnAPwZDr13e-3m0LpRBUO"
+        file_id = "1pGK7NuociQRSv9MLL-pUkrcR43eCxj7L"
         gdown.download(f"https://drive.google.com/uc?id={file_id}", temp_model_path, quiet=False)
-        class_list = ['IT_PC/기기', 'IT_게임', 'IT_과학', 'IT_모바일', 'IT_보안', 'IT_비즈니스/정책',
-        'IT_인터넷/SNS', 'IT_콘텐츠', '건강/의료', '경제', '경제/산업', '과학/테크놀로지', '교육',
-        '국제', '날씨', '노동', '동물', '문화', '미디어', '사건/사법', '사회', '사회_일반', '스포츠',
-        '여성', '오피니언/사설', '장애인', '정치', '환경']
+        class_list = ['IT_PC/기기','IT_게임','IT_과학','IT_모바일','IT_보안','IT_비즈니스/정책','IT_인터넷/SNS',
+        'IT_콘텐츠','건강/의료', '경제/산업','과학/테크놀로지','교육','노동','동물','문화','사건/사법','스포츠',
+        '오피니언/사설','정치','환경']
         model = BertForSequenceClassification.from_pretrained('skt/kobert-base-v1', num_labels=len(class_list))
         model.load_state_dict(torch.load(temp_model_path, map_location=device))
         
@@ -99,10 +98,9 @@ def category_update(collection):
 
     # KoBERT 로드
     tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
-    class_list = ['IT_PC/기기', 'IT_게임', 'IT_과학', 'IT_모바일', 'IT_보안', 'IT_비즈니스/정책',
-        'IT_인터넷/SNS', 'IT_콘텐츠', '건강/의료', '경제', '경제/산업', '과학/테크놀로지', '교육',
-        '국제', '날씨', '노동', '동물', '문화', '미디어', '사건/사법', '사회', '사회_일반', '스포츠',
-        '여성', '오피니언/사설', '장애인', '정치', '환경']
+    class_list = ['IT_PC/기기','IT_게임','IT_과학','IT_모바일','IT_보안','IT_비즈니스/정책','IT_인터넷/SNS',
+    'IT_콘텐츠','건강/의료', '경제/산업','과학/테크놀로지','교육','노동','동물','문화','사건/사법','스포츠',
+    '오피니언/사설','정치','환경']
     # 레이블의 수를 고유한 클래스 수로 설정 (이전 학습 시 사용한 것과 동일해야 함)
     num_labels = len(class_list)  # 예시로 5개로 설정, 실제 학습 시 사용한 클래스 수로 변경
 
--- summary_update.py
+++ summary_update.py
@@ -13,73 +13,67 @@ from dotenv import load_dotenv
 import logging
 from datetime import datetime
 from news_preprocess.keyword import keyword_update
+import torch
+from transformers import AutoModel, AutoTokenizer
+from sklearn.metrics.pairwise import cosine_similarity
+from kss import split_sentences
 
-# KoBART 모델과 토크나이저 불러오기
-tokenizer = PreTrainedTokenizerFast.from_pretrained("ainize/kobart-news")
-model = BartForConditionalGeneration.from_pretrained("ainize/kobart-news")
+import numpy as np
+import networkx as nx
 
-def split_text(input_list, partition):
-    """
-    입력 텍스트 리스트를 파티션 단위로 분할합니다.
-    """
-    if partition == 1:
-        return [' '.join(input_list)]
-    
-    size = len(input_list) // partition
-    # 오버랩을 추가해 분리된 문맥이 더 자연스럽게 연결되도록 함
-    return [
-        ' '.join(input_list[max(0, i * size - 2):(i + 1) * size])
-        for i in range(partition)
-    ]
+# 모델 및 토크나이저 로드
 
-def summary(row):
-    """
-    뉴스 텍스트를 요약하는 함수.
-    """
-    try:
-        paragraphs = row.split('\n')
-        input_list = []
+def embed_sentences(sentences, tokenizer, model):
+    inputs = tokenizer(sentences, return_tensors='pt', padding=True, truncation=True, max_length=4096)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    # CLS 토큰의 임베딩을 사용
+    return outputs.last_hidden_state[:, 0, :].numpy()
 
-        # '다.'를 기준으로 문장 분리 후 리스트에 추가
-        for paragraph in paragraphs:
-            if '다.' in paragraph:
-                sentences = re.findall(r'.*?다\.', paragraph)
-                input_list.extend(sentences)
+def extractive_summary(document, num_sentences=3):
+    model_name = 'monologg/kobigbird-bert-base'
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    sentence_embeddings = embed_sentences(document, tokenizer, model)
 
-        # 텍스트 길이에 따라 파티션 분할
-        partition = max(1, len(row) // 1800 + 1)
-        input_list = split_text(input_list, partition)
+    # 문장 간 유사도 행렬 계산
+    sim_matrix = cosine_similarity(sentence_embeddings)
 
-        summary_first = ''
-        for text in input_list:
-            input_ids = tokenizer.encode(text, truncation=True, return_tensors='pt')
-            summary_ids = model.generate(
-                input_ids=input_ids,
-                length_penalty=1.0,
-                max_length=250,
-                min_length=56,
-                num_beams=4
-            )
-            summary_first += tokenizer.decode(summary_ids[0], skip_special_tokens=True) + ' '
+    # 중요 문장 선택 (여기서는 가장 중앙에 위치한 문장 선택)
+    nx_graph = nx.from_numpy_array(sim_matrix)
+    scores = nx.pagerank(nx_graph)
 
-        # 최종 요약 수행
-        if len(input_list) > 1:
-            input_ids = tokenizer.encode(summary_first.strip(), truncation=True, return_tensors='pt')
-            final_summary_ids = model.generate(
-                input_ids=input_ids,
-                length_penalty=1.0,
-                max_length=300,
-                min_length=56,
-                num_beams=4
-            )
-            full_summary = tokenizer.decode(final_summary_ids[0], skip_special_tokens=True)
-        else:
-            full_summary = summary_first.strip()
+    # 중요 문장 선택
+    ranked_sentences = sorted(((scores[i], s) for i, s in enumerate(document)), reverse=True)
+    if len(ranked_sentences) > num_sentences:
+      summary_list = [ranked_sentences[i][1] for i in range(num_sentences)]
+    else:
+      summary_list = [ranked_sentences[i][1] for i in range(len(ranked_sentences))]
 
-        return summary_first.strip(), full_summary
-    except Exception as e:
-        print(f"요약 중 오류 발생: {e}")
-        return None, None
+    summary = ' '.join(summary_list)
+    return summary
+
+def news_summary_extraction(document):
+    document = document.split('\n')
+    document_list = []
+    for i in document:
+      document_list.extend(split_sentences(i))
+    summary = extractive_summary(document_list, num_sentences=3)
+    return summary
+
+def news_summary_abstraction(chunk):
+    model = BartForConditionalGeneration.from_pretrained('/app/belab_scraping/news_preprocess/summary_pretrained_model')
+    tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-base-v1')
+    inputs = tokenizer(chunk, return_tensors='pt', max_length=1024, truncation=True)
+    summary_ids = model.generate(
+        inputs['input_ids'],
+        max_length=150,
+        num_beams=4,
+        early_stopping=True,
+        no_repeat_ngram_size=3  # 3-gram 반복 방지
+    )
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
 
 def update_news_summary(collection):
     """
@@ -87,29 +81,22 @@ def update_news_summary(collection):
     """
     # 요약되지 않은 뉴스 항목 가져오기
 
-    news_data = collection.find({"full_summary": {"$exists": False}})
+    news_data = collection.find({"abtraction": {"$exists": False}})
 
     for news in news_data:
         news_id = news["_id"]
         content = news.get("news_content", "")
-
         if not content.strip():
             print(f"뉴스 {news_id}의 콘텐츠가 비어 있습니다.")
             continue
+        try:
+            extraction_summary = news_summary_extraction(content)
+            abtraction_summary = news_summary_abstraction(extraction_summary)
 
-        # 뉴스 콘텐츠 요약
-        first_summary, full_summary = summary(content)
-
-        if full_summary:
-            try:
-                # MongoDB에 요약 업데이트
-                collection.update_one(
-                    {"_id": news_id},
-                    {"$set": {"first_summary": first_summary, "full_summary": full_summary}}
-                )
-                print(f"뉴스 {news_id} 요약 완료")
-            except Exception as e:
-                print(f"뉴스 {news_id} 업데이트 중 오류 발생: {e}")
+            # MongoDB 문서 업데이트
+            collection.update_one({'_id': row['_id']}, {'$set': {'extraction': extraction_summary,'abtraction':abtraction_summary}})
+        except Exception as e:
+            print(f"Error occurred: {e}")
 
 def total_update():
     load_dotenv()
