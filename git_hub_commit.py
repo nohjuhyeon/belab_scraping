b45e876cb3fd6b49c402128cb35aade4751bfea0
nohjuhyeon <njh2720@gmail.com>
2025-03-31 17:26:00 +0900

    Auto Commit.

--- git_hub_commit.py
+++ git_hub_commit.py
@@ -2,65 +2,6 @@ import os
 import subprocess
 from datetime import datetime
 
-def git_commit_and_push():
-    # 환경 변수에서 폴더 경로 가져오기
-    folder_path = os.environ.get("folder_path")
-    if not folder_path:
-        print("Error: 'folder_path' 환경 변수가 설정되지 않았습니다.")
-        return
-
-    # Git 작업을 수행하는 디렉토리로 이동
-    os.chdir(folder_path)
-    
-    # 현재 시간 출력
-    print(f"현재 시간: {datetime.now()}")
-
-    try:
-        # Git Pull
-        print("Pulling latest changes from the remote repository...")
-        subprocess.run(["git", "pull"], check=True)
-
-        # Git Add
-        print("Staging changes...")
-        subprocess.run(["git", "add", "."], check=True)
-
-        folder_path = os.environ.get("folder_path")
-        script_path = folder_path+"function_list/git_workflow.sh"
-        argument = "Auto Commit."
-        print(datetime.now())
-
-        try:
-            # 스크립트 실행
-            result = subprocess.run(
-                [script_path, argument],
-                capture_output=True,  # 표준 출력과 표준 오류를 캡처
-                text=True,            # 출력을 문자열로 처리
-                check=True            # 명령어 실패 시 예외 발생
-            )
-        except subprocess.CalledProcessError as e:
-            # Git 명령 실행 중 오류 발생 시 메시지 출력
-            print(f"An error occurred: {e}")
-            print("Git 작업 중 오류가 발생했습니다.")
-
-        # Git Push
-        print("Pushing changes to the remote repository...")
-        subprocess.run(["git", "push"], check=True)
-
-        print("Pull, commit, and push complete!")
-    except subprocess.CalledProcessError as e:
-        # Git 명령 실행 중 오류 발생 시 메시지 출력
-        print(f"An error occurred: {e}")
-        print("Git 작업 중 오류가 발생했습니다.")
-
-# 스크립트를 직접 실행할 경우 git_commit_and_push() 함수 호출
-if __name__ == "__main__":
-    git_commit_and_push()
-
-
-import os
-import subprocess
-from datetime import datetime
-
 def git_commit():
     folder_path = os.environ.get("folder_path")
     script_path = folder_path+"function_list/git_workflow.sh"
